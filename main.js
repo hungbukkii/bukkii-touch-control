befc1c6d053fa8942003f7897314a18351a4aa89
Hung Nguyen <hung@bukkii.app>
2025-12-17 16:17:13 +0700

    v7

--- main.js
+++ main.js
@@ -12,7 +12,14 @@ const robot = require("robotjs");
 
 let mainWindow;
 
+const { autoUpdater } = require("electron-updater");
+
+// Configure logging
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+
 function createWindow() {
+  // ... existing window creation code ...
   const primaryDisplay = screen.getPrimaryDisplay();
   const { width, height } = primaryDisplay.workAreaSize;
   const { x, y } = primaryDisplay.bounds;
@@ -53,6 +60,7 @@ function createWindow() {
 }
 
 function createTray() {
+  // ... existing tray code ...
   const iconPath = path.join(__dirname, "icon-transparent.png");
   tray = new Tray(iconPath);
   const contextMenu = Menu.buildFromTemplate([
@@ -73,11 +81,38 @@ app.whenReady().then(() => {
   createWindow();
   createTray();
 
+  // Check for updates
+  autoUpdater.checkForUpdatesAndNotify();
+
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
+});
+
+/* New Update Events */
+autoUpdater.on('checking-for-update', () => {
+  console.log('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+  console.log('Update available.', info);
+});
+autoUpdater.on('update-not-available', (info) => {
+  console.log('Update not available.', info);
+});
+autoUpdater.on('error', (err) => {
+  console.log('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  console.log(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+  console.log('Update downloaded');
+  // Auto install on quit is default, so no extra code strictly needed unless we want to prompt immediately
 });
 
 app.on("window-all-closed", () => {
